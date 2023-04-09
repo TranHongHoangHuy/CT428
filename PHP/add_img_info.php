3861cb4b4609ed8fe3962f67f85dff7d3c181542
thanh <ntthanh335@gmail.com>
2023-04-09 14:05:42 +0700

    a

--- PHP/add_img_info.php
+++ PHP/add_img_info.php
@@ -3,27 +3,18 @@ include '../PHP/conn.php';
 // Nếu form đã submit, thực hiện lưu thông tin sản phẩm vào CSDL
 if (isset($_POST['submit'])) {
     // Lấy thông tin sản phẩm từ form
-    $id_catalog = $_POST['id_catalog'];
-    $productName = $_POST['productName'];
-    $productOption = $_POST['productOption'];
-    $price = $_POST['price'];
-    $discout = $_POST['discout'];
-    $image_link = $_POST['image_link'];
-    $content = $_POST['content'];
+    $id_product = $_POST['id_product'];
+    $url = $_POST['url'];
+
 
     // Chuẩn bị câu truy vấn SQL để thêm sản phẩm vào CSDL
-    $sql = "INSERT INTO `product` (`id_catalog`, `productName`, `productOption`, `price`, `discout`, `image_link`, `content`) VALUES (:id_catalog, :productName, :productOption, :price, :discout, :image_link, :content)";
+    $sql = "INSERT INTO `img_info` (`id_product`, `url_info`) VALUES (:id_product, :url)";
     $stmt = $pdo->prepare($sql);
 
     // Thực hiện truy vấn SQL với thông tin sản phẩm vừa lấy từ form
     $stmt->execute([
-        ':id_catalog' => $id_catalog,
-        ':productName' => $productName,
-        ':productOption' => $productOption,
-        ':price' => $price,
-        ':discout' => $discout,
-        ':image_link' => $image_link,
-        ':content' => $content
+        ':id_product' => $id_product,
+        ':url' => $url
     ]);
 
     // Hiển thị thông báo thành công và xóa thông tin trong form
@@ -31,9 +22,10 @@ if (isset($_POST['submit'])) {
     $_POST = array();
 }
 // Lấy thông tin danh mục sản phẩm để hiển thị trong form
-$sql = "SELECT * FROM `catalog`";
+$sql = "SELECT * FROM `product`";
 $stmt = $pdo->query($sql);
-$catalogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -63,45 +55,26 @@ $catalogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
     <link href="../ASSETS/IMG/favicon.ico" rel="icon" type="image/x-icon" />
-    <title>Nhập SP</title>
+    <title>Nhập img</title>
 </head>
 
 <body>
     <div class="container">
-        <h1>Thêm sản phẩm mới</h1>
+        <h1>Thêm img mới</h1>
         <form method="post" action="">
             <div class="form-group">
-                <label for="id_catalog">Danh mục sản phẩm:</label>
-                <select class="form-control" id="id_catalog" name="id_catalog">
-                    <?php foreach ($catalogs as $catalog) : ?>
-                        <option value="<?= $catalog['id_catalog'] ?>"><?= $catalog['catalogName'] ?></option>
+                <label for="id_product">Danh mục sản phẩm:</label>
+                <select class="form-control" id="id_product" name="id_product">
+                    <?php foreach ($products as $product) : ?>
+                        <option value="<?= $product['id_product'] ?>"><?= $product['productName'] ?></option>
                     <?php endforeach; ?>
                 </select>
             </div>
             <div class="form-group">
-                <label for="productName">Tên sản phẩm:</label>
-                <input type="text" class="form-control" id="productName" name="productName" placeholder="Nhập tên sản phẩm" required>
+                <label for="url">Link ảnh sản phẩm:</label>
+                <input type="text" class="form-control" id="url" name="url" placeholder="Nhập link ảnh sản phẩm" required>
             </div>
-            <div class="form-group">
-                <label for="productOption">Tùy chọn sản phẩm:</label>
-                <input type="text" class="form-control" id="productOption" name="productOption" placeholder="Nhập tùy chọn sản phẩm" required>
-            </div>
-            <div class="form-group">
-                <label for="price">Giá sản phẩm:</label>
-                <input type="number" class="form-control" id="price" name="price" placeholder="Nhập giá sản phẩm" required>
-            </div>
-            <div class="form-group">
-                <label for="discout">Giảm giá sản phẩm:</label>
-                <input type="number" class="form-control" id="discout" name="discout" placeholder="Nhập giảm giá sản phẩm">
-            </div>
-            <div class="form-group">
-                <label for="image_link">Link ảnh sản phẩm:</label>
-                <input type="text" class="form-control" id="image_link" name="image_link" placeholder="Nhập link ảnh sản phẩm" required>
-            </div>
-            <div class="form-group">
-                <label for="content">Mô tả sản phẩm:</label>
-                <textarea class="form-control" id="content" name="content" rows="5" placeholder="Nhập mô tả sản phẩm" required></textarea>
-            </div>
+
             <button type="submit" name="submit" class="btn btn-primary">Thêm sản phẩm</button>
         </form>
     </div>
--- PHP/manager.php
+++ PHP/manager.php
@@ -103,50 +103,7 @@ $result = $pdo->query($sql);
     </div>
     <div class="col-sm-4">
       <h3>Quản lý sản phẩm</h3>
-
-      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <label for="id">ID:</label>
-  <input type="number" id="id" name="id" value="<?php echo $id; ?>"><br>
-  <label for="tsp">Tên sản phẩm:</label>
-  <input type="text" id="tsp" name="tsp" value="<?php echo $tsp; ?>"><br>
-  <label for="gia">Giá:</label>
-  <input type="number" id="gia" name="gia" value="<?php echo $gia; ?>"><br>
-  <label for="loai">Loại:</label>
-  <input type="number" id="loai" name="loai" value="<?php echo $loai; ?>"><br>
-  <label for="giam">Giảm giá:</label>
-    <select id="giam" name="giam" >
-      <option value="<?php echo $giam; ?>">không giảm</option>
-      <option value="<?php echo $giam; ?>">giảm 5%</option>
-      <option value="<?php echo $giam; ?>">giảm 10%</option>
-      <option value="<?php echo $giam; ?>">giảm 20%</option>
-      <option value="<?php echo $giam; ?>">giảm 50%</option>   
-    </select><br>
-  <label for="anh">Ảnh:</label>
-  <input type="text" id="anh" name="anh"><br>
-  <label for="luachon">Lụa chọn:</label>
-  <input type="text" id="luachon" name="luachon"><br>
-  <label for="note">Ghi chú:</label>
-  <input type="text" id="note" name="note"><br>
-  <button type="submit" >Thêm sản phẩm</button>
-        </form>
-<?php
-$id = $_POST["id"];
-$name = $_POST["tsp"];
-$gia=$_POST["gia"];
-$loai=$_POST["loai"];
-$giam=$_POST["giam"];
-$ghichu =$_POST["note"];
-$luachon=$_POST["luachon"];
-$anh=$_POST["anh"];
-include '../PHP/conn.php';
-$add="INSERT INTO product (productName, price, id_catalog, discout, productOption,image_link,content) 
-VALUES ('$name', '$gia', '$loai','$giam','$luachon','$anh','$ghichu')";
-if($pdo->errorCode()){
-  echo "<script>alert('ok')";
-}else{
-  echo "<script>alert('no ok')";
-}
-?>
+<?php include '../PHP/add_product.php';?>
     </div>
   </div>
 </div>
